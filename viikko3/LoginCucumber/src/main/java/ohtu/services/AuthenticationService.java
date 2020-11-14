16d86826e74612ab3ae5ddfdbfb9c910e33aab0f
Anastasia Diseth <annadiseth@gmail.com>
2020-11-14 21:54:12 +0200

    Tehtävä 6

--- viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -40,7 +40,22 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
-
-        return false;
+        if (username.length() < 3 || password.length() < 8) return true;
+        char[] uname = username.toCharArray();
+        for (int i = 0; i < uname.length; i++) {
+            if (!Character.isLetter(uname[i]) && !Character.isLowerCase(uname[i])) {
+                return true;
+            }
+        }
+        boolean eipelkkakirjaimia = false;
+        char[] pw = password.toCharArray();
+        for (int i = 0; i < pw.length; i++) {
+            if (!Character.isLetter(pw[i])) {
+                eipelkkakirjaimia = true;
+                System.out.println("detected notLetters");
+            }
+        }
+        if (eipelkkakirjaimia) return false;
+        return true;
     }
 }
